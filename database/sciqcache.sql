b47fd9ddb4af73140be98748e3fd95feb997a3a3
maillard <flavien.maillard.91@gmail.com>
2025-05-30 11:37:56 +0200

    update: sql database usable by sciqlop-cache

--- database/sciqcache.sql
+++ database/sciqcache.sql
@@ -1,5 +1,8 @@
-CREATE TABLE IF NOT EXISTS cache (
-        key TEXT PRIMARY KEY,
-        value TEXT,
-        timestamp INTEGER
-    );
+CREATE TABLE IF NOT EXISTS `cache` (
+    `id` INT unsigned NOT NULL AUTO_INCREMENT,
+    `key` TEXT PRIMARY KEY NOT NULL COMMENT "The key is the path to the cache item from .cache/",
+    `value` BLOB NOT NULL,
+    `last_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `last_month_request_count` INT unsigned NOT NULL DEFAULT 0,
+    `this_month_request_count` INT unsigned NOT NULL DEFAULT 0,
+);
