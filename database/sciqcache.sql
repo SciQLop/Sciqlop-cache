36511e12b7dafad4bd75f95f54bf185e20588e68
maillard <flavien.maillard.91@gmail.com>
2025-06-03 12:04:50 +0200

    fix: correct primary key and remove COMMENT unsupported by sqlite3

--- database/sciqcache.sql
+++ database/sciqcache.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `cache` (
-    `id` INT unsigned NOT NULL AUTO_INCREMENT,
-    `key` TEXT PRIMARY KEY NOT NULL COMMENT "The key is the path to the cache item from .cache/",
+    `id` INT PRIMARY KEY unsigned NOT NULL AUTO_INCREMENT,
+    `key` TEXT NOT NULL,--"The key is the path to the cache item from .cache/"
     `value` BLOB NOT NULL,
     `last_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     `last_month_request_count` INT unsigned NOT NULL DEFAULT 0,
