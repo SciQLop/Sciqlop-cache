13f31537f4c23611e678517589ba9622169b116c
maillard <flavien.maillard.91@gmail.com>
2025-06-23 15:05:30 +0200

    update: store time as double

--- database/sciqcache.sql
+++ database/sciqcache.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS `cache` (
     `key` TEXT NOT NULL,--"The key is the path to the cache item from .cache/"
     `path` TEXT NOT NULL,
     `value` BLOB NOT NULL,
-    'expire' DOUBLE NOT NULL DEFAULT -1, -- -1 means never expire
-    `last_update` DOUBLE NOT NULL DEFAULT 0,
-    `last_use` DOUBLE NOT NULL DEFAULT 0,
+    'expire' DOUBLE unsigned DEFAULT NULL, -- NULL means never expire
+    `last_update` DOUBLE unsigned NOT NULL DEFAULT 0,
+    `last_use` DOUBLE unsigned NOT NULL DEFAULT 0,
     `access_count_since_last_update` INT unsigned NOT NULL DEFAULT 0,
 --    `this_month_access_count` INT unsigned NOT NULL DEFAULT 0,
 );
