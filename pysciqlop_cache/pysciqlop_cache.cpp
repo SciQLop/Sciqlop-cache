5d24e3f30ff10669e15f942672696c1aaaa52574
maillard <flavien.maillard.91@gmail.com>
2025-06-23 15:07:31 +0200

    include Cache class and its function in pysciqlop_cache module

--- pysciqlop_cache/pysciqlop_cache.cpp
+++ pysciqlop_cache/pysciqlop_cache.cpp
@@ -9,6 +9,8 @@
 
 #include <fmt/ranges.h>
 
+#include "include/sciqlop_cache/sciqlop_cache.hpp"
+
 namespace py = pybind11;
 
 PYBIND11_MODULE(_pysciqlop_cache, m)
@@ -19,11 +21,18 @@ PYBIND11_MODULE(_pysciqlop_cache, m)
 
     )pbdoc";
 
-    m.def("test", test);
-    m.def("sum",
-        [](py::bytes& buffer)
-        {
-            py::buffer_info info(py::buffer(buffer).request());
-            return sum(static_cast<char*>(info.ptr), static_cast<std::size_t>(info.size));
-        });
+    py::class_<Cache>(m, "Cache")
+        .def(py::init<const std::string &, size_t>(), py::arg("db_path") = "sciclop-cache.db", py::arg("max_size") = 1000)
+        .def("open", &Cache::open, py::arg("db_path") = "sciclop-cache.db")
+        .def("close", &Cache::close)
+        .def("set", &Cache::set, py::arg("key"), py::arg("value"), py::arg("expire") = 3600)
+        .def("get", &Cache::get, py::arg("key"))
+        .def("add", &Cache::add, py::arg("key"), py::arg("value"), py::arg("expire") = 3600)
+        .def("del", &Cache::del, py::arg("key"))
+        .def("pop", &Cache::pop, py::arg("key"))
+        .def("touch", &Cache::touch, py::arg("key"), py::arg("expire") = 3600)
+        .def("expire", &Cache::expire)
+        .def("evict", &Cache::evict)
+        .def("clear", &Cache::clear)
+        .def("check", &Cache::check);
 }
