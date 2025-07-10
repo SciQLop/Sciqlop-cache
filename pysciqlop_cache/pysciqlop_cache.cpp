48aec82efa375b4615b0bd02b05ff8f373dd3a3e
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-07-10 11:37:16 +0200

    Add pythonic dict interface

--- pysciqlop_cache/pysciqlop_cache.cpp
+++ pysciqlop_cache/pysciqlop_cache.cpp
@@ -25,29 +25,31 @@ PYBIND11_MODULE(_pysciqlop_cache, m)
     py::class_<Cache>(m, "Cache")
         .def(py::init<const std::string&, size_t>(), py::arg("db_path") = "sciclop-cache.db",
             py::arg("max_size") = 1000)
+        .def("count", &Cache::count)
+        .def("__len__", &Cache::count)
         .def(
             "set",
             [](Cache& c, const std::string& key, const std::string& value,
-                std::chrono::system_clock::duration expire)
-            { return c.set(key, value, expire); },
-            py::arg("key"), py::arg("value"),
-            py::arg("expire") = 3600s)
+                std::chrono::system_clock::duration expire) { return c.set(key, value, expire); },
+            py::arg("key"), py::arg("value"), py::arg("expire") = 3600s)
+        .def(
+            "__setitem__", [](Cache& c, const std::string& key, const std::string& value)
+            { return c.set(key, value); }, py::arg("key"), py::arg("value"))
         .def("get", &Cache::get, py::arg("key"))
-        .def("add",
-             [](Cache& c, const std::string& key, const std::string& value,
-             std::chrono::system_clock::duration expire)
-            {
-                return c.add(key, value, expire);
-            }
-             , py::arg("key"), py::arg("value"), py::arg("expire") = 3600s)
+        .def(
+            "__getitem__", &Cache::get, py::arg("key"))
+        .def("keys", &Cache::keys)
+        .def(
+            "add",
+            [](Cache& c, const std::string& key, const std::string& value,
+                std::chrono::system_clock::duration expire) { return c.add(key, value, expire); },
+            py::arg("key"), py::arg("value"), py::arg("expire") = 3600s)
         .def("del", &Cache::del, py::arg("key"))
         .def("pop", &Cache::pop, py::arg("key"))
-        .def("touch",
-             [&](Cache& c, const std::string& key, std::chrono::system_clock::duration expire)
-             {
-                 return c.touch(key, expire);
-             }
-             , py::arg("key"), py::arg("expire") = 3600s)
+        .def(
+            "touch",
+            [&](Cache& c, const std::string& key, std::chrono::system_clock::duration expire)
+            { return c.touch(key, expire); }, py::arg("key"), py::arg("expire") = 3600s)
         .def("expire", &Cache::expire)
         .def("evict", &Cache::evict)
         .def("clear", &Cache::clear)
