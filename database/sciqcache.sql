ef88d95d3095034401ebbab7276bed6a48bd651e
maillard <flavien.maillard.91@gmail.com>
2025-07-25 17:08:17 +0200

    fix: value or path can be null depending on storing emplacement

--- database/sciqcache.sql
+++ database/sciqcache.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `cache` (
     `id` INT PRIMARY KEY unsigned NOT NULL AUTO_INCREMENT,
-    `key` TEXT NOT NULL,--"The key is the path to the cache item from .cache/"
-    `path` TEXT NOT NULL,
-    `value` BLOB NOT NULL,
+    `key` TEXT NOT NULL, -- "The key is the path to the cache item from .cache/"
+    `path` TEXT DEFAULT NULL, -- if NULL, the value is stored in the database
+    `value` BLOB DEFAULT NULL, -- if NULL, the value is stored in the file system
     'expire' DOUBLE unsigned DEFAULT NULL, -- NULL means never expire
     `last_update` DOUBLE unsigned NOT NULL DEFAULT 0,
     `last_use` DOUBLE unsigned NOT NULL DEFAULT 0,
